592c25ffe0b50ff97ae27ac2ea7c1a16794f2871
Antony Jaibu <antonyjaibuofficial@gmail.com>
2026-08-17 14:28:57 +0530

    api.jss  changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL =
-  import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
+  import.meta.env.VITE_API_URL || 'https://mini-contact-book.onrender.com/api';
 
 const api = axios.create({
   baseURL: BASE_URL,

baef375874286bbc634bfdb4c9e40049dd212e2a
Antony Jaibu <antonyjaibuofficial@gmail.com>
2026-08-17 14:11:35 +0530

    frontend components  changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
+
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
 
 const api = axios.create({
